734809f1005a41f3c498a82b78c7f65208dc3235
atharv9910gupta-coder <atharv9910gupta@gmail.com>
2025-12-07 22:39:37 +0530

    Rename parameter from 'prompt' to 'messages'

--- modules/groq_client.py
+++ modules/groq_client.py
@@ -1,15 +1,12 @@
 import os
 from groq import Groq
 
-def run_groq_chat(prompt):
+def run_groq_chat(messages):
     client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-    chat_completion = client.chat.completions.create(
+    completion = client.chat.completions.create(
         model="llama-3.1-8b-instant",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+        messages=messages
     )
 
-    return chat_completion.choices[0].message["content"]
-
+    return completion.choices[0].message["content"]
