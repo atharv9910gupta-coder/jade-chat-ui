ea9fa6fee3c67609998c4363a4adca1f13972b0e
atharv9910gupta-coder <atharv9910gupta@gmail.com>
2025-12-07 22:47:34 +0530

    Implement dummy email and SMS sender functions
    Added dummy functions for sending email and SMS.

--- modules/tools.py
+++ modules/tools.py
@@ -1,1 +1,19 @@
+import streamlit as st
+
+# Dummy email sender (works without any API)
+def send_email(to, subject, body):
+    # In real version you will add: SMTP or API (SendGrid, Mailgun, etc.)
+    print("Email Sent!")
+    print("To:", to)
+    print("Subject:", subject)
+    print("Body:", body)
+    return True
+
+# Dummy SMS sender (works without any API)
+def send_sms(number, message):
+    # In real version you will add: Twilio or SMS API
+    print("SMS Sent!")
+    print("Number:", number)
+    print("Message:", message)
+    return True
 
