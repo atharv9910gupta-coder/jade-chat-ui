50032441bc0b49a8b85cfc537090b9da9a728bfc
atharv9910gupta-coder <atharv9910gupta@gmail.com>
2025-12-08 16:00:36 +0530

    Update Email.py

--- pages/Email.py
+++ pages/Email.py
@@ -1,20 +1,4 @@
 import streamlit as st
-from modules.actions import send_email
 
-def email_page():
-    st.title("📧 Send Email")
-
-    to = st.text_input("Send To:")
-    subject = st.text_input("Subject:")
-    body = st.text_area("Message:")
-
-    st.write("### Sender Email Login")
-    sender_email = st.text_input("Your Email:")
-    sender_pass = st.text_input("Your Email Password:", type="password")
-
-    if st.button("Send Email"):
-        if not to or not subject or not body:
-            st.warning("Fill all fields!")
-        else:
-            result = send_email(to, subject, body, sender_email, sender_pass)
-            st.info(result)
+st.title("📧 Email System")
+st.info("Email system setup coming soon!")
